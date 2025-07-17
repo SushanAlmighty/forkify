28db2f39a7f5dd712b2996dd29264c9db94770f2
QA-Engg <tempjira96@gmail.com>
2025-07-17 17:49:52 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const API_KEY = 'e470378c-6ab3-4a60-a79a-5919f3a8d8d1';
+
+export const MODAL_CLOSE_SEC = 2.5;
